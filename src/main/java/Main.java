9f2291ef71771436522555c1c3210b2e22ed4770
papertnzz <papertnyy@gmail.com>
2026-04-18 17:05:10 +0200

    feat: initial Maven project with Roman numeral converter

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+      String input = scanner.nextLine();
+    } catch (Exception e) {
+      System.out.println("Input error");
+    }
+  }
+
+  public static String toRoman(int num) {
+    if (num < 1 || num > 3000) {
+      throw new IllegalArgumentException("Number must be between 1 and 3000");
+    }
+    int[] values = {
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1
+    };
+    String[] numerals = {
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"
+    };
+    StringBuilder result = new StringBuilder();
+    for (int i = 0; i < values.length; i++) {
+      while (num >= values[i]) {
+        num -= values[i];
+        result.append(numerals[i]);
+      }
+    }
+    return result.toString();
+  }
+
+}
